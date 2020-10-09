f13e1c9fd2284b7da4018c310d277de6a08ee6fb
Cicrocoft <mailtilmig@gmail.com>
2020-10-09 07:06:27 +0200

    Update AutoSkills.lua

--- AutoSkills.lua
+++ AutoSkills.lua
@@ -1,57 +1,64 @@
-AutoSkills =
-{
-    ["WARRIOR"] = {
-        { Level = 4, MinimumMoney = 210, Skills = { "Battle Shout_1", "Charge_1", "Rend_1" } },
-        { Level = 8, MinimumMoney = 200, Skills = { "Heroic Strike_2" } },
-        { Level = 10, MinimumMoney = 1200, Skills = { "Bloodrage", "Rend_2" } },
-        { Level = 12, MinimumMoney = 2000, Skills = { "Battle Shout_2", "Overpower_1" } },
-    },
-    ["PALADIN"] = {
-        { Level = 4, MinimumMoney = 210, Skills = { "Devotion Aura_1", "Blessing of Might_1", "Judgement" } },
-        { Level = 6, MinimumMoney = 200, Skills = { "Divine Protection_1", "Holy Light_2" } },
-        { Level = 8, MinimumMoney = 200, Skills = { "Hammer of Justice_1", "Purify" } },
-        { Level = 10, MinimumMoney = 900, Skills = { "Devotion Aura_2", "Lay on Hands_1", "Seal of Righteousness_2" } },
-    },
-    ["HUNTER"] = {
-        { Level = 4, MinimumMoney = 200, Skills = { "Aspect of the Monkey", "Serpent Sting_1" } },
-        { Level = 6, MinimumMoney = 200, Skills = { "Arcane Shot_1", "Hunter's Mark_1" } },
-        { Level = 8, MinimumMoney = 400, Skills = { "Concussive Shot", "Raptor Strike_2", "Parry" } },
-        { Level = 10, MinimumMoney = 800, Skills = { "Serpent Sting_2", "Aspect of the Hawk_1" } },
-    },
-    ["ROGUE"] = {
-        { Level = 4, MinimumMoney = 110, Skills = { "Stealth_1", "Pick Pocket" } },
-        { Level = 6, MinimumMoney = 200, Skills = { "Gouge_1", "Sinister Strike_2" } },
-        { Level = 8, MinimumMoney = 400, Skills = { "Evasion", "Eviscerate_2" } },
-        { Level = 10, MinimumMoney = 600, Skills = { "Slice and Dice_1", "Sprint_1" } },
-    },
-    ["PRIEST"] = {
-        { Level = 4, MinimumMoney = 210, Skills = { "Power Word: Fortitude_1", "Lesser Heal_2", "Shadow Word: Pain_1" } },
-        { Level = 6, MinimumMoney = 200, Skills = { "Power Word: Shield_1", "Smite_2" } },
-        { Level = 8, MinimumMoney = 200, Skills = { "Renew_1" } },
-        { Level = 10, MinimumMoney = 1200, Skills = { "Lesser Heal_3", "Mind Blast_1", "Shadow Word: Pain_2", "Resurrection_1" } },
-    },
-    ["SHAMAN"] = {
-        { Level = 4, MinimumMoney = 210, Skills = { "Rockbiter Weapon_1", "Earth Shock_1" } },
-        { Level = 6, MinimumMoney = 100, Skills = { "Healing Wave_2" } },
-        { Level = 8, MinimumMoney = 400, Skills = { "Earth Shock_2", "Lightning Bolt_2", "Lightning Shield_1", "Rockbiter Weapon_2" } },
-        { Level = 10, MinimumMoney = 400, Skills = { "Flame Shock_1" } },
-    },
-    ["MAGE"] = {
-        { Level = 4, MinimumMoney = 210, Skills = { "Arcane Intellect_1", "Conjure Water_1", "Frostbolt_1" } },
-        { Level = 6, MinimumMoney = 300, Skills = { "Fire Blast_1", "Conjure Food_1", "Fireball_2" } },
-        { Level = 8, MinimumMoney = 400, Skills = { "Frostbolt_2", "Polymorph_1" } },
-        { Level = 10, MinimumMoney = 1200, Skills = { "Conjure Water_2", "Frost Armor_2", "Frost Nova_1" } },
-    },
-    ["WARLOCK"] = {
-        { Level = 4, MinimumMoney = 110, Skills = { "Immolate_1", "Corruption_1" } },
-        { Level = 6, MinimumMoney = 200, Skills = { "Life Tap_1", "Shadow Bolt_2" } },
-        { Level = 8, MinimumMoney = 400, Skills = { "Fear_1", "Curse of Agony_1" } },
-        { Level = 10, MinimumMoney = 1200, Skills = { "Create Healthstone (Minor)", "Demon Skin_2", "Drain Soul_1", "Immolate_2" } },
-    },
-    ["DRUID"] = {
-        { Level = 4, MinimumMoney = 210, Skills = { "Mark of the Wild_1", "Moonfire_1", "Rejuvenation_1" } },
-        { Level = 6, MinimumMoney = 200, Skills = { "Thorns_1", "Wrath_2" } },
-        { Level = 8, MinimumMoney = 200, Skills = { "Healing Touch_2" } },
-        { Level = 10, MinimumMoney = 900, Skills = { "Mark of the Wild_2", "Moonfire_2", "Rejuvenation_2" } },
-    },
+AutoSkills =
+{
+    ["WARRIOR"] = {
+        { Level = 4, MinimumMoney = 210, Skills = { "Battle Shout_1", "Charge_1", "Rend_1" } },
+        { Level = 8, MinimumMoney = 200, Skills = { "Heroic Strike_2" } },
+        { Level = 10, MinimumMoney = 1200, Skills = { "Bloodrage", "Rend_2" } },
+        { Level = 12, MinimumMoney = 2000, Skills = { "Battle Shout_2", "Overpower_1" } },
+    },
+    ["PALADIN"] = {
+        { Level = 4, MinimumMoney = 210, Skills = { "Devotion Aura_1", "Blessing of Might_1", "Judgement" } },
+        { Level = 6, MinimumMoney = 200, Skills = { "Divine Protection_1", "Holy Light_2" } },
+        { Level = 8, MinimumMoney = 200, Skills = { "Hammer of Justice_1", "Purify" } },
+        { Level = 10, MinimumMoney = 900, Skills = { "Devotion Aura_2", "Lay on Hands_1", "Seal of Righteousness_2" } },
+    },
+    ["HUNTER"] = {
+        { Level = 4, MinimumMoney = 200, Skills = { "Aspect of the Monkey", "Serpent Sting_1" } },
+        { Level = 6, MinimumMoney = 200, Skills = { "Arcane Shot_1", "Hunter's Mark_1" } },
+        { Level = 8, MinimumMoney = 400, Skills = { "Concussive Shot", "Raptor Strike_2" } },
+        { Level = 10, MinimumMoney = 800, Skills = { "Serpent Sting_2", "Aspect of the Hawk_1" } },
+    },
+    ["ROGUE"] = {
+        { Level = 4, MinimumMoney = 110, Skills = { "Stealth_1", "Pick Pocket" } },
+        { Level = 6, MinimumMoney = 200, Skills = { "Gouge_1", "Sinister Strike_2" } },
+        { Level = 8, MinimumMoney = 400, Skills = { "Evasion", "Eviscerate_2" } },
+        { Level = 10, MinimumMoney = 600, Skills = { "Slice and Dice_1", "Sprint_1" } },
+    },
+    ["PRIEST"] = {
+        { Level = 4, MinimumMoney = 210, Skills = { "Power Word: Fortitude_1", "Lesser Heal_2", "Shadow Word: Pain_1" } },
+        { Level = 6, MinimumMoney = 200, Skills = { "Power Word: Shield_1", "Smite_2" } },
+        { Level = 8, MinimumMoney = 200, Skills = { "Renew_1" } },
+        { Level = 10, MinimumMoney = 1200, Skills = { "Lesser Heal_3", "Mind Blast_1", "Shadow Word: Pain_2", "Resurrection_1" } },
+    },
+    ["SHAMAN"] = {
+        { Level = 4, MinimumMoney = 210, Skills = { "Rockbiter Weapon_1", "Earth Shock_1" } },
+        { Level = 6, MinimumMoney = 100, Skills = { "Healing Wave_2" } },
+        { Level = 8, MinimumMoney = 400, Skills = { "Earth Shock_2", "Lightning Bolt_2", "Lightning Shield_1", "Rockbiter Weapon_2" } },
+        { Level = 10, MinimumMoney = 400, Skills = { "Flame Shock_1" } },
+    },
+    ["MAGE"] = {
+        { Level = 4, MinimumMoney = 210, Skills = { "Arcane Intellect_1", "Conjure Water_1", "Frostbolt_1" } },
+        { Level = 6, MinimumMoney = 300, Skills = { "Fire Blast_1", "Conjure Food_1", "Fireball_2" } },
+        { Level = 8, MinimumMoney = 400, Skills = { "Frostbolt_2", "Polymorph_1" } },
+        { Level = 10, MinimumMoney = 1200, Skills = { "Conjure Water_2", "Frost Armor_2", "Frost Nova_1" } },
+		{ Level = 12, MinimumMoney = 1200, Skills = { "Conjure Food_2", "Dampen Magic_1" } },
+		{ Level = 14, MinimumMoney = 2700, Skills = { "Arcane Intellect_2", "Fire Blast_2", "Frostbolt_3" } },
+		{ Level = 20, MinimumMoney = 15800, Skills = { "Remove Lesser Curse", "Conjure Water_3", "Blink", "Evocation", "Mana Shield_1", "Polymorph_2", "Frostbolt_4", "Frost Armor_3" } },	
+		{ Level = 22, MinimumMoney = 6000, Skills = { "Fire Blast_3", "Conjure Food_3" } },			
+		{ Level = 26, MinimumMoney = 23000, Skills = { "Counterspell", "Dampen Magic_2", "Cone of Cold_1", "Frost Nova_2", "Frostbolt_5" } },	
+		{ Level = 28, MinimumMoney = 21000, Skills = { "Arcane Intellect_3", "Conjure Mana Agate_1", "Mana Shield_2" } },		
+		{ Level = 30, MinimumMoney = 32000, Skills = { "Conjure Water_3", "Fire Blast_4", "Mana Shield_2", "Ice Armor_1" } },				
+    },
+    ["WARLOCK"] = {
+        { Level = 4, MinimumMoney = 110, Skills = { "Immolate_1", "Corruption_1" } },
+        { Level = 6, MinimumMoney = 200, Skills = { "Life Tap_1", "Shadow Bolt_2" } },
+        { Level = 8, MinimumMoney = 400, Skills = { "Fear_1", "Curse of Agony_1" } },
+        { Level = 10, MinimumMoney = 1200, Skills = { "Create Healthstone (Minor)", "Demon Skin_2", "Drain Soul_1", "Immolate_2" } },
+    },
+    ["DRUID"] = {
+        { Level = 4, MinimumMoney = 210, Skills = { "Mark of the Wild_1", "Moonfire_1", "Rejuvenation_1" } },
+        { Level = 6, MinimumMoney = 200, Skills = { "Thorns_1", "Wrath_2" } },
+        { Level = 8, MinimumMoney = 200, Skills = { "Healing Touch_2" } },
+        { Level = 10, MinimumMoney = 900, Skills = { "Mark of the Wild_2", "Moonfire_2", "Rejuvenation_2" } },
+    },
 }
